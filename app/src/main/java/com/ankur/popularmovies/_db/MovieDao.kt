12712a47b9898d1ba9a308bbd238a7e3f95a9de3
Ankur <guptaankur.gupta0@gmail.com>
2019-06-14 15:52:33 +0530

    MovieDao, make dao for room database

--- app/src/main/java/com/ankur/popularmovies/_db/MovieDao.kt
+++ app/src/main/java/com/ankur/popularmovies/_db/MovieDao.kt
@@ -1,4 +1,10 @@
 package com.ankur.popularmovies._db
 
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
 interface MovieDao {
+  @Query("DELETE FROM popular_movies")
+  fun clearTable()
 }
