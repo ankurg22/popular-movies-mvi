1ee40086a2a0c6812fe9eab475dd8b42eaa7681d
Ankur <guptaankur.gupta0@gmail.com>
2019-06-18 18:19:03 +0530

    PopularMoviesView, method extraction

--- app/src/main/java/com/ankur/popularmovies/PopularMoviesView.kt
+++ app/src/main/java/com/ankur/popularmovies/PopularMoviesView.kt
@@ -9,37 +9,42 @@ import com.ankur.popularmovies._repository.Error
 interface PopularMoviesView {
   fun render(state: PopularMoviesState) {
     when (state.fetchAction) {
-      IN_PROGRESS -> {
-        if (state.movies.isEmpty()) {
-          showProgress(true)
-        } else {
-          showResults(state.movies)
-          showSilentProgress(true)
-        }
-      }
+      IN_PROGRESS -> renderFetchInProgress(state)
 
-      FETCH_FAILED -> {
-        showProgress(false)
-        showSilentProgress(false)
-        state.error?.let { showError(it) }
-      }
+      FETCH_FAILED -> renderFetchFailed(state)
 
-      FETCH_SUCCESSFUL -> {
-        val hasMovies = state.searchedMovies.isNotEmpty() and state.movies.isNotEmpty()
-        if (hasMovies) {
-          showResults(state.searchedMovies)
-        } else {
-          showProgress(false)
-          showSilentProgress(false)
-          showResults(state.movies)
-        }
-      }
+      FETCH_SUCCESSFUL -> renderFetchSuccessful(state)
+    }
+  }
+
+  fun renderFetchSuccessful(state: PopularMoviesState) {
+    val hasMovies = state.searchedMovies.isNotEmpty() and state.movies.isNotEmpty()
+    if (hasMovies) {
+      showResults(state.searchedMovies)
+    } else {
+      showProgress(false)
+      showSilentProgress(false)
+      showResults(state.movies)
+    }
+  }
+
+  fun renderFetchFailed(state: PopularMoviesState) {
+    showProgress(false)
+    showSilentProgress(false)
+    state.error?.let { showError(it) }
+  }
+
+  fun renderFetchInProgress(state: PopularMoviesState) {
+    if (state.movies.isEmpty()) {
+      showProgress(true)
+    } else {
+      showResults(state.movies)
+      showSilentProgress(true)
     }
   }
 
   // View access
   fun showResults(movies: List<Movie>)
-
   fun showProgress(show: Boolean)
   fun showSilentProgress(show: Boolean)
   fun showError(error: Error)
