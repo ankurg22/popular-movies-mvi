b4b753b4240522de2567667e1f0833a21d27e8ad
Ankur <guptaankur.gupta0@gmail.com>
2019-06-19 18:11:00 +0530

    AppModule, add database and moviesapi method

--- app/src/main/java/com/ankur/popularmovies/_db/PopularMoviesDatabase.kt
+++ app/src/main/java/com/ankur/popularmovies/_db/PopularMoviesDatabase.kt
@@ -1,25 +1,10 @@
 package com.ankur.popularmovies._db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.ankur.popularmovies._http.Movie
-
-const val DATABASE_NAME = "movies"
 
 @Database(entities = [Movie::class], version = 1, exportSchema = true)
 abstract class PopularMoviesDatabase : RoomDatabase() {
   abstract fun movieDao(): MovieDao
-
-  companion object {
-    fun getInstance(context: Context): RoomDatabase.Builder<PopularMoviesDatabase> {
-      return Room
-        .databaseBuilder(
-          context,
-          PopularMoviesDatabase::class.java,
-          DATABASE_NAME
-        )
-    }
-  }
 }
--- app/src/main/java/com/ankur/popularmovies/_di/AppModule.kt
+++ app/src/main/java/com/ankur/popularmovies/_di/AppModule.kt
@@ -2,16 +2,24 @@ package com.ankur.popularmovies._di
 
 import android.app.Application
 import android.content.Context
+import androidx.room.Room
+import com.ankur.popularmovies.BuildConfig
 import com.ankur.popularmovies._db.PopularMoviesDatabase
 import com.ankur.popularmovies._http.MoviesApi
-import com.ankur.popularmovies._http.MoviesClient
+import com.ankur.popularmovies._http.RxThreadCallAdapterFactory
 import com.ankur.popularmovies._repository.PopularMoviesRepository
 import com.ankur.popularmovies._repository.PopularMoviesRepositoryImpl
 import com.ankur.popularmovies._repository.SchedulerProvider
 import com.ankur.popularmovies._repository.SchedulerProviderImpl
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
+private const val DATABASE_NAME = "movies"
 
 @Module
 class AppModule(private val application: Application) {
@@ -27,8 +35,15 @@ class AppModule(private val application: Application) {
 
   @Provides
   @Singleton
-  fun provideMoviesDatabase(context: Context): PopularMoviesDatabase =
-      PopularMoviesDatabase.getInstance(context).build()
+  fun provideMoviesDatabase(context: Context): PopularMoviesDatabase {
+    return Room
+        .databaseBuilder(
+            context,
+            PopularMoviesDatabase::class.java,
+            DATABASE_NAME
+        )
+        .build()
+  }
 
   @Provides
   @Singleton
@@ -36,7 +51,22 @@ class AppModule(private val application: Application) {
 
   @Provides
   @Singleton
-  fun provideMoviesApi(): MoviesApi = MoviesClient.getInstance().create(MoviesApi::class.java)
+  fun provideMoviesApi(retrofit: Retrofit): MoviesApi {
+    return retrofit
+        .create(MoviesApi::class.java)
+  }
+
+  @Provides
+  @Singleton
+  fun provideRetrofit(): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addCallAdapterFactory(RxThreadCallAdapterFactory())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build()
+  }
 
   @Provides
   @Singleton
